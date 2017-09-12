67a04d2a539b30545da8fa61f0eab5dcc21c995c
admin <admin@FASTDRAG>
2017-09-12 12:06:25 +0300

    dto import fix

--- EnerpriseProjectArchetype/src/main/resources/archetype-resources/__rootArtifactId__-dto/src/main/java/__packageInPathFormat__/dto/UserDto.java
+++ EnerpriseProjectArchetype/src/main/resources/archetype-resources/__rootArtifactId__-dto/src/main/java/__packageInPathFormat__/dto/UserDto.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import ru.sendto.dto.Dto;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
